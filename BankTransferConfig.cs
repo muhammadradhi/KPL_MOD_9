1d5a3cfb9a687749db5f1051492a8e934d5e94c8
muhammadradhi <radhirafi06@gmail.com>
2026-05-02 15:11:36 +0700

    aplikasi bank transfer

---
+++ BankTransferConfig.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul9_103022400055
+{
+    internal class BankTransferConfig
+    {
+        public Config config;
+        public const string filepath = @"bank_transfer_config.json";
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            } catch
+            {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+
+        public void ReadConfigFile()
+        {
+            string read = File.ReadAllText(filepath);
+            config = JsonSerializer.Deserialize<Config>(read);
+        }
+
+        public void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string write = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filepath, write);
+        }
+
+        public void SetDefault()
+        {
+            config = new Config();
+            config.lang = "en";
+            Transfer transfer = new Transfer(25000000, 6500, 15000);
+            config.transfer = transfer;
+            List<string> files = new List<string>();
+            files.Add("RTO (real-time)");
+            files.Add("SKN");
+            files.Add("RTGS");
+            files.Add("BI FAST");
+
+            config.methods = files;
+            Confirmation confirmation = new Confirmation();
+            confirmation.en = "yes";
+            confirmation.id = "ya";
+            config.confirmation = confirmation;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static Program;
+using System.Text.Json;
+using System.Numerics;
+using System.Net.Sockets;
+using Modul9_103022400055;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        if (config.config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        } else if (config.config.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di transfer:");
+        }
+
+        double input2 = Convert.ToInt32(Console.ReadLine());
+        double total;
+    }
+}
